7d3746b657f58be734674805d2e1ec54372f47e1
Andrew R <beta@runelite.bot>
2024-08-09 14:10:57 -0500

    chore: remove boilerplate code from template

--- src/game/scenes/preloader.ts
+++ src/game/scenes/preloader.ts
@@ -8,7 +8,6 @@ export class Preloader extends Scene {
 	init() {
 		this.add.image(512, 384, 'background');
 		this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
-
 		const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
 		this.load.on('progress', (progress: number) => {
@@ -18,15 +17,12 @@ export class Preloader extends Scene {
 
 	preload() {
 		this.load.setPath('assets');
-
 		this.load.image('logo', 'logo.png');
 	}
 
 	create() {
 		//  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
 		//  For example, you can define global animations here, so we can use them in other scenes.
-
-		//  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
 		this.scene.start('MainMenu');
 	}
 }
