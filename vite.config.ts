047249a1d4e47a26f3fe1a017001dbacf0bafa5d
Andrew R <beta@runelite.bot>
2024-09-24 10:40:24 -0500

    add minification & improve chunking for phaser

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ mode }) => ({
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					phaser: ['phaser']
+				}
+			}
+		},
+		minify: mode === 'production' ? 'terser' : false,
+		terserOptions: {
+			compress: {
+				passes: 2
+			},
+			mangle: true,
+			format: {
+				comments: false
+			}
+		}
+	}
+}));
