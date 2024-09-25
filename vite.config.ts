010dfaa6f2a4c17b29720e9d9c664a5dc8bca370
Andrew R <beta@runelite.bot>
2024-09-25 14:32:55 -0500

    remove broken build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					phaser: ['phaser']
-				}
-			}
-		},
-		minify: mode === 'production' ? 'terser' : false,
-		terserOptions: {
-			compress: {
-				passes: 2
-			},
-			mangle: true,
-			format: {
-				comments: false
-			}
-		}
-	}
+	plugins: [sveltekit()]
 }));
