5e5e04b9042f86c4f00794cd65c59e8b91f10f22
Andrew R <beta@runelite.bot>
2024-09-25 12:42:41 -0500

    add size prop to in-use icons

--- src/components/icons/chat.svelte
+++ src/components/icons/chat.svelte
@@ -1,4 +1,8 @@
-<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
+<script lang="ts">
+	let { size = 18 } = $props();
+</script>
+
+<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size}>
 	<path
 		d="M20 2H2v20h2V4h16v12H6v2H4v2h2v-2h16V2h-2zM6 7h12v2H6V7zm8 4H6v2h8v-2z"
 		fill="currentColor"
