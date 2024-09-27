5dddcb510d85bb7eb7845bc364bce9ce22489b15
Andrew R <beta@runelite.bot>
2024-09-27 03:37:05 -0500

    anims

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ import daisyui from 'daisyui';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			animation: {
+				'pulse-glow': 'pulse-glow 2s ease-in-out infinite'
+			}
+		}
 	},
-	plugins: [daisyui]
+	plugins: [daisyui],
+	daisyui: {
+		themes: [
+			'forest',
+			'halloween',
+			'dim',
+			'sunset',
+			'pastel',
+			'fantasy',
+			'lofi',
+			'retro',
+			'emerald'
+		]
+	}
 };
