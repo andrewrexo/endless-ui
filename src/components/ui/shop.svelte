cb57e400f6e68a5edefdd448ddee224cac5ebe5a
Andrew R <beta@runelite.bot>
2024-09-27 13:53:16 -0500

    update shop layout to use daisy modal

--- src/components/ui/shop.svelte
+++ src/components/ui/shop.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { ui } from '$lib/user-interface.svelte';
-	import { fade, fly } from 'svelte/transition';
+	import { fade, fly, scale } from 'svelte/transition';
 	import Card from '../icons/card.svelte';
 	import Close from '../icons/close.svelte';
 	import Coin from '../icons/coin.svelte';
@@ -13,6 +13,17 @@
 	}
 
 	let shopCount = $state(8);
+	let sendBehind = $state(false);
+
+	$effect(() => {
+		if (!ui.interfaces.shop) {
+			setTimeout(() => {
+				sendBehind = true;
+			}, 200);
+		} else {
+			sendBehind = false;
+		}
+	});
 
 	let rarityToColorMap = $state(
 		new Map([
@@ -23,22 +34,18 @@
 		])
 	);
 
-	let items: ShopItem[] = $state([
-		{
-			name: 'Withered Staff',
-			description: "yeah this is a shitty staff but it's all you can afford",
-			price: 25,
-			rarity: 'Common'
-		}
-	]);
-
-	$effect(() => {
-		items = Array.from({ length: shopCount }, (_, i) => ({
+	let items: ShopItem[] = $state(
+		Array.from({ length: shopCount }, (_, i) => ({
 			name: `Item ${i + 1}`,
 			description: `Description for Item ${i + 1}`,
 			price: Math.floor(Math.random() * 100) + 1,
 			rarity: ['common', 'uncommon', 'rare', 'epic'][Math.floor(Math.random() * 4)]
-		}));
+		}))
+	);
+
+	$effect(() => {
+		const shopModal = document.getElementById('shop-modal') as HTMLDialogElement;
+		ui.interfaces.shop && shopModal ? shopModal.showModal() : shopModal.close();
 	});
 </script>
 
@@ -55,14 +62,9 @@
 	</div>
 {/snippet}
 
-{#snippet shopItemDescription(description: string)}
-	<span class="text-xs text-base-content/70">{description} </span>
-{/snippet}
-
 {#snippet shopItem(item: ShopItem)}
 	<button
-		transition:fly={{ duration: 100, y: -100 }}
-		class="btn flex h-16 w-full gap-4 rounded-lg bg-base-100 p-2 transition-all duration-300 hover:scale-[1.02] hover:bg-base-content/20"
+		class="btn flex h-fit w-full min-w-[200px] gap-4 rounded-lg bg-base-100 p-2 transition-all duration-300 hover:scale-[1.02] hover:bg-base-content/20"
 	>
 		<div class="flex w-full gap-4">
 			<div class="flex h-12 w-16 items-center justify-center rounded-lg bg-base-300/50">
@@ -77,50 +79,53 @@
 						{@render shopItemPrice(item.price)}
 					</div>
 				</div>
-				<div class="flex w-full justify-between font-light">
-					{@render shopItemDescription(item.description)}
-					{@render shopItemRarity(item.rarity)}
+				<div class="flex h-full w-full justify-between text-left font-light">
+					<span class="text-xs text-base-content/70">{item.description} </span>
 				</div>
 			</div>
 		</div>
 	</button>
 {/snippet}
 
-<div
-	class="pointer-events-auto absolute left-[80px] top-[105px] z-50 flex h-[380px] w-4/5 flex-col justify-between rounded-lg bg-neutral p-3 transition-all duration-300"
-	transition:fade={{ duration: 200 }}
->
-	<div class="header flex justify-between pb-2">
-		<div class="flex gap-2">
-			<Card size={20} />
-			<div class="title text-xl">
-				<span class="text-primary/70">Jim's</span>
-				Shop
+<dialog id="shop-modal" class:-z-50={sendBehind} class="modal">
+	<div
+		class="modal-box pointer-events-auto z-50 flex min-w-[600px] flex-col justify-between gap-2 rounded-lg bg-neutral p-3"
+	>
+		<div class="header flex justify-between pb-2">
+			<div class="flex gap-2">
+				<Card size={20} />
+				<div class="title text-xl">
+					<span class="text-primary/70">Jim's</span>
+					Shop
+				</div>
+			</div>
+			<div class="flex gap-2">
+				<button
+					class="animate-spin-active btn btn-circle btn-xs rounded-full bg-base-content/20 p-1 focus:ring-0 focus-visible:ring-0 active:ring-0"
+					aria-label="Close shop"
+					onclick={() => {
+						ui.handleButtonAction('shop', 'close');
+					}}
+				>
+					<Close />
+				</button>
 			</div>
 		</div>
-		<div class="flex gap-2">
-			<button
-				class="animate-spin-active btn btn-circle btn-xs rounded-full bg-base-content/20 p-1 focus:ring-0 active:ring-0"
-				aria-label="Close shop"
-				onclick={() => {
-					ui.handleButtonAction('shop', 'close');
-				}}
-			>
-				<Close />
+		<div class="shop-items grid w-full grid-cols-2 gap-2">
+			{#each items as item}
+				{@render shopItem(item)}
+			{/each}
+		</div>
+		<div class="mt-auto flex justify-end pt-1">
+			<button class="btn h-8 min-h-8 bg-base-200 text-lg hover:scale-105 hover:bg-base-100">
+				Buy
 			</button>
 		</div>
 	</div>
-	<div class="shop-items grid w-full grid-cols-2 gap-2">
-		{#each items as item}
-			{#key items.length >= 0}
-				{@render shopItem(item)}
-			{/key}
-		{/each}
-	</div>
-	<div class="mt-auto flex justify-end">
-		<button class="text-md btn-neutral/30 btn h-8 min-h-8 text-xl hover:scale-105">Buy</button>
-	</div>
-</div>
+	<form method="dialog" class="modal-backdrop">
+		<button onclick={() => ui.handleButtonAction('shop', 'close')}>close</button>
+	</form>
+</dialog>
 
 <style>
 	.header,
