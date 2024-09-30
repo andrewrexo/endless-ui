70ef711b9a73ce6238473c2f0687a448f4e4daf9
Andrew R <beta@runelite.bot>
2024-09-29 23:46:51 -0500

    add devtools command to tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
+	tauri::Builder::default()
+		.invoke_handler(tauri::generate_handler![open_devtools])
+		.run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn open_devtools(window: tauri::Window) {
+  #[cfg(debug_assertions)]
+  window.open_devtools();
 }
