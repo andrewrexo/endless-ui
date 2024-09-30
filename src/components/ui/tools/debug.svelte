84a6d4ced493af8a8c3ed927f72341c1bae50426
Andrew R <beta@runelite.bot>
2024-09-29 23:47:19 -0500

    update debug component to use panel

--- src/components/ui/tools/debug.svelte
+++ src/components/ui/tools/debug.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import { ui } from '$lib/user-interface.svelte';
-	import { fly } from 'svelte/transition';
 	import { EventBus } from '../../../game/event-bus';
-
-	let {
-		dragAction,
-		position
-	}: { dragAction: (node: HTMLElement) => void; position: { x: number; y: number } } = $props();
+	import { invoke } from '@tauri-apps/api/tauri';
+	import Panel from '../primitives/panel.svelte';
+	import Button from '../primitives/button.svelte';
 
 	const refreshBrowser = () => {
 		window.location.reload();
@@ -19,35 +16,21 @@
 	const openModal = () => {
 		ui.handleButtonAction('shop', 'open');
 	};
+
+	const openBrowserDevtools = () => {
+		if ('__TAURI__' in window) {
+			invoke('open_devtools');
+			return;
+		}
+
+		// This will trigger a breakpoint if DevTools is open
+		debugger;
+	};
 </script>
 
-<div
-	use:dragAction
-	transition:fly={{ duration: 300, x: -20 }}
-	style="position: absolute; left: {position.x}px; top: {position.y}px;"
-	class="pointer-events-auto flex w-[140px] flex-col gap-1 rounded-lg bg-base-300/80 p-1"
->
-	<span class="px-1 text-sm font-bold tracking-wider">Debug</span>
-
-	<button onclick={refreshBrowser}>
-		<span class="text-accent">Refresh</span>
-		<span class="text-accent">client</span>
-	</button>
-	<button onclick={refreshScene} class="text-accent">
-		<span class="">Refresh</span>
-		<span class="">scene</span>
-	</button>
-	<button onclick={openModal} class=""> Open modal </button>
-</div>
-
-<style>
-	button {
-		font-family: 'Abaddon';
-		@apply btn btn-sm text-sm font-light text-accent;
-	}
-
-	div > span {
-		font-family: 'Abaddon';
-		@apply text-sm font-bold;
-	}
-</style>
+<Panel componentId="debug" className="p-1">
+	<Button onclick={refreshBrowser}>Refresh client</Button>
+	<Button onclick={refreshScene}>Refresh scene</Button>
+	<Button onclick={openModal}>Open modal</Button>
+	<Button onclick={openBrowserDevtools}>Open Devtools</Button>
+</Panel>
