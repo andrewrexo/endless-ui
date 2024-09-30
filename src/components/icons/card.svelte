454721bd47eda31b22ba49251be7e09127f2209b
Andrew R <beta@runelite.bot>
2024-09-29 23:48:12 -0500

    update icon

--- src/components/icons/card.svelte
+++ src/components/icons/card.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-	let { size = 16 } = $props();
+	let { size = 16, class: className = '' } = $props();
 </script>
 
-<svg width={size} height={size} fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<svg
+	class={className}
+	width={size}
+	height={size}
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+>
 	<path
 		d="M2 4h18v2H4v2h16v4H4v6h10v2H2V4zm20 0h-2v8h2V4zm-4 10h2v2h2v2h-2v2h-2v-2h-2v-2h2v-2z"
 		fill="currentColor"
