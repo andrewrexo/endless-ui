5423baaed476203f1c6ff1780233960dd84deb9d
Andrew R <beta@runelite.bot>
2024-10-03 23:56:33 -0500

    update to tauri 2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![open_devtools])
-		.run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![open_devtools])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
-async fn open_devtools(window: tauri::Window) {
-  #[cfg(debug_assertions)]
-  window.open_devtools();
+async fn open_devtools(window: tauri::WebviewWindow) {
+    #[cfg(debug_assertions)]
+    window.open_devtools();
 }
